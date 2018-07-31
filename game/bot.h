4438a5625f1f630f147993a640540fc1688174cc
Bartosz Pajewski <bartoszpajewski@gmail.com>
2018-07-31 13:01:16 +0200

    Rozbudowa cz 3

--- game/bot.h
+++ game/bot.h
@@ -8,6 +8,16 @@ class Bot
 public:
     Bot();
 
+    void setHP(int value){hp=std::move(value);}
+    void setShield(int value){shield=std::move(value);}
+    void setDamage(int value){dmg=std::move(value);}
+    void setMovement(int value){movement=std::move(value);}
+    void setEnergy(int value){energy=std::move(value);}
+    void setEnergyLoad(int value){energyLoad=std::move(value);}
+    void setExpierience(int value){expierience=std::move(value);}
+    void setCost(int value){cost=std::move(value);}
+
+
 private:
     int hp;
     int shield;
